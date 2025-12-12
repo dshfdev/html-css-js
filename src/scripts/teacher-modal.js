f8f8a681dbf697ada802924efe274edd3044fe21
Daria Sh <dsh.f.dev@gmail.com>
2025-12-12 18:10:28 +0300

    feat: add function updateSelectBtnText

--- src/scripts/teacher-modal.js
+++ src/scripts/teacher-modal.js
@@ -2,6 +2,8 @@ const tabButtons = document.querySelectorAll('.teacher-tab-buttons__btn');
 const tabContents = document.querySelectorAll('.teacher-tab-content');
 const closeModalBtn = document.querySelector('.modal__close-btn');
 const modalBackdrop = document.querySelector('.modal__backdrop');
+const selectOptions = document.querySelectorAll('[role="option"]');
+const selectBtnText = document.querySelector('.select__selected-text');
 
 const setButtonActive = (clickedButton) => {
   tabButtons.forEach((button) => {
@@ -25,12 +27,23 @@ const setContentActive = (dataTab) => {
   tabContent.scrollTo({ top: 0 });
 };
 
+const updateSelectBtnText = (dataTab) => {
+  const matchingOption = Array.from(selectOptions).find(
+    (option) => option.getAttribute('data-tab') === dataTab,
+  );
+
+  if (matchingOption) {
+    selectBtnText.innerText = matchingOption.innerText.trim();
+  }
+};
+
 tabButtons.forEach((button) => {
   button.addEventListener('click', () => {
     setButtonActive(button);
 
     const tab = button.getAttribute('data-tab');
     setContentActive(tab);
+    updateSelectBtnText(tab);
   });
 });
 
@@ -39,5 +52,6 @@ tabButtons.forEach((button) => {
     const firstButton = tabButtons[0];
     setButtonActive(firstButton);
     setContentActive(firstButton.getAttribute('data-tab'));
+    updateSelectBtnText(firstButton.getAttribute('data-tab'));
   });
 });
